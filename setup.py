a74be8006f9a79e9d47055eaba03ee7de54b0256
Nir Soffer <nsoffer@redhat.com>
2019-10-09 21:54:51 +0300

    Release 0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="userstorage",
-    version="0.2",
+    version="0.3",
     author="Nir Soffer",
     author_email="nirsof@gmail.com",
     description="Create storage for tests",
